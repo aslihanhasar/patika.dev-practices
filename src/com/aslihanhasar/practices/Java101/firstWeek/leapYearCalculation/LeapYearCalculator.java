b81777771905949cac92e481f80f25535c27fd70
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-25 21:26:15 +0300

    Add project via upload

---
+++ src/com/aslihanhasar/practices/Java101/firstWeek/leapYearCalculation/LeapYearCalculator.java
@@ -0,0 +1,28 @@
+package com.aslihanhasar.practices.Java101.firstWeek.leapYearCalculation;
+
+import java.util.Scanner;
+
+public class LeapYearCalculator {
+    public static void main(String[] args) {
+        int year;
+        boolean isLeapYear = false;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        year = scanner.nextInt();
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    isLeapYear = true;
+                }
+            } else {
+                isLeapYear = true;
+            }
+        }
+        if (isLeapYear) {
+            System.out.println("This year is a Leap Year.");
+        } else {
+            System.out.print("This year is not a Leap Year.");
+        }
+    }
+}
