941758faa2f7769836e3aadfebfc5a2619c25af8
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-26 14:33:11 +0300

    Add project via upload

---
+++ src/com/aslihanhasar/practices/Java101/firstWeek/recursivePrime/PrimeNumbers.java
@@ -0,0 +1,34 @@
+package com.aslihanhasar.practices.Java101.firstWeek.recursivePrime;
+
+import java.util.Scanner;
+
+public class PrimeNumbers {
+    static boolean isPrime(int number, int i) {
+        if (number == 2) {
+            return true;
+        }
+        if (number == i) {
+            return true;
+        }
+        if (number % i == 0) {
+            return false;
+        }
+        return isPrime(number, i + 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number, i = 2;
+
+        do {
+            System.out.print("Enter a number: ");
+            number = scanner.nextInt();
+
+            if (isPrime(number, i)) {
+                System.out.println("This number is a prime number.");
+            } else {
+                System.out.println("This number is not a prime number.");
+            }
+        } while (number >= 0);
+    }
+}
