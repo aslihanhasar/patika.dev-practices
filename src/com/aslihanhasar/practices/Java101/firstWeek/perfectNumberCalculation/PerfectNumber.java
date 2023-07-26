653389210cf353b70c728807a2539d01da801a8e
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-26 14:33:57 +0300

    Add project via upload

---
+++ src/com/aslihanhasar/practices/Java101/firstWeek/perfectNumberCalculation/PerfectNumber.java
@@ -0,0 +1,28 @@
+package com.aslihanhasar.practices.Java101.firstWeek.perfectNumberCalculation;
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number, i;
+        do {
+            System.out.print("Enter a number: ");
+            number = scanner.nextInt();
+            int sum = 0;
+            for (i = 1; i < number; i++) {
+                if (number % i == 0) {
+                    sum += i;
+                }
+            }
+            if ((sum == number) && (number > 0)) {
+                System.out.println("This number is a perfect number.");
+
+            } else if ((!(sum == number) && (number > 0))) {
+                System.out.println("This number is not a perfect number.\n" +
+                        "Try a different number.");
+            }
+        } while (number > 0);
+        System.out.println("Invalid Input! Enter a positive number.");
+    }
+}
