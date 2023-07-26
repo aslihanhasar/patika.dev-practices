72f7ec0a54dcfe404476256bd667c50eb2540494
Aslıhan Hasar <aslihan_hasar@hotmail.com>
2023-07-26 16:28:38 +0300

    Add project via upload

---
+++ src/com/aslihanhasar/practices/Java101/secondWeek/sortingArrayElements/Main.java
@@ -0,0 +1,39 @@
+package com.aslihanhasar.practices.Java101.secondWeek.sortingArrayElements;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    static int[] receivingArr() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the length of the array: ");
+
+        int lengthArr = scanner.nextInt();
+        int[] arr = new int[lengthArr];
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter the " + (i + 1) + ". element of the array: ");
+            arr[i] = scanner.nextInt();
+        }
+        return arr;
+    }
+
+    static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                int tempN;
+                if (arr[j + 1] < arr[j]) {
+                    tempN = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = tempN;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = receivingArr();
+        bubbleSort(arr);
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+    }
+}
